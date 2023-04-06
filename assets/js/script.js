c7938278b36052a4138bd83ca5773f6b035845e7
Alexandre D <derriere.alexandre0103@gmail.com>
2023-04-06 15:50:52 +0200

    last of the day

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function onLoad(){
+
+    const title1 = document.querySelectorAll(".bot-top");
+    title1.forEach(title=>{
+        title.classList.add("translate-y-0")
+        title.classList.add("opacity-100")
+    });
+    const title2 = document.querySelectorAll(".left-right");
+    title2.forEach(title=>{
+    title.classList.add("translate-x-0")
+    title.classList.add("opacity-100")
+    });
+    
+    }
+    window.onLoad = setTimeout(onLoad, 800);
+
+    
