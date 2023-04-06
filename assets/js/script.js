9f9a759e2b0359b450c819e2cd71a327bb0498c0
Nabil BELLILA <bellilanabil8@gmail.com>
2023-04-06 12:24:13 +0200

    nouveaux stakeboard grid + animation title

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function onLoad(){
+const title1 = document.querySelector("#bot-top");
+title1.classList.add("translate-y-0")
+title1.classList.add("opacity-100")
+const title2 = document.querySelector("#left-right");
+title2.classList.add("translate-x-20")
+title2.classList.add("opacity-100")
+
+}
+
+window.onLoad = setTimeout(onLoad, 800);
+
+
+
+
