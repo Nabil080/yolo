c54606b89d625ae78d1e3e114cb455443f5b54ed
Nabil BELLILA <bellilanabil8@gmail.com>
2023-04-06 12:44:34 +0200

    début hover card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 function onLoad(){
-const title1 = document.querySelector("#bot-top");
-title1.classList.add("translate-y-0")
-title1.classList.add("opacity-100")
-const title2 = document.querySelector("#left-right");
-title2.classList.add("translate-x-20")
-title2.classList.add("opacity-100")
+
+const title1 = document.querySelectorAll(".bot-top");
+title1.forEach(title=>{
+    title.classList.add("translate-y-0")
+    title.classList.add("opacity-100")
+});
+const title2 = document.querySelectorAll(".left-right");
+title2.forEach(title=>{
+title.classList.add("translate-x-0")
+title.classList.add("opacity-100")
+});
 
 }
-
 window.onLoad = setTimeout(onLoad, 800);
-
-
-
-
